63cc3d09b540b6d76c30b0e49f05d1a65eaf6233
lazar-the-coder <120413005+lazar-the-coder@users.noreply.github.com>
2022-12-16 10:45:10 -0600

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -14,23 +14,6 @@ import random
 high_scores = []
 
 def start_game():
-    """Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
     print("***Welcome to the number guessing game***\n***We will chose a number***\n***Try and guess what it is***")
     play_game()
 
@@ -74,6 +57,4 @@ def victory(player_name, guess_count):
         else:
             print("I didn't understand your answer")
 
-
-# Kick off the program by calling the start_game function.
 start_game()
