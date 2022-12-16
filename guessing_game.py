4651156c1ad05721da8df04998fd44b337f1e439
lazar-the-coder <120413005+lazar-the-coder@users.noreply.github.com>
2022-12-16 13:58:15 -0600

    Added and fixed High score shower
    Making sure I have it down

--- guessing_game.py
+++ guessing_game.py
@@ -6,7 +6,8 @@ Project 1 - Number Guessing Game
 
 import random
 
-high_scores = []
+previous_scores = []
+high_score = [100]
 #you can change the range here, thus adjusting the difficulty
 min_number = 1
 max_number = 16
@@ -22,9 +23,11 @@ def play_game():
     #gets a number in the range
     answer_number = random.randint(min_number, max_number)
     guess_count = 0
+    print("The previous heigh score was {}".format(high_score[0]))
     #Makes sure the loop starts by setting the guess not equal to answer
     guess_number = (answer_number - 1)
     print("The machine is thinking of a number between {} and {}".format(min_number, max_number))
+    #When the guess matches, the loop stops
     while guess_number != answer_number:
         try:
             guess_number = int(input("Choose an integer  "))
@@ -35,16 +38,22 @@ def play_game():
             elif guess_number < answer_number:
                 print("It's higher")
             guess_count += 1
+        #if the guess isn't an integer.
         except ValueError:
             print("thats not a integer")
     victory(player_name, guess_count)
 
 def victory(player_name, guess_count):
     print("Got it")
-    high_scores.append([player_name, guess_count])
+    #sets the new high score
+    if guess_count < high_score[0]:
+        high_score[0] = guess_count
+    #adds scores to score list
+    previous_scores.append([player_name, guess_count])
     print("***Congratulations on guessing the right number***\n***It took you", guess_count, "guesses***")
-    print("***Here are the high scores***")
-    for score in high_scores:
+    #prints previous scores.
+    print("***Here are the previous scores***")
+    for score in previous_scores:
         print(score[0], ":", score[1])
     while True:
         answer = input("Want to play again? (Y/N)")
